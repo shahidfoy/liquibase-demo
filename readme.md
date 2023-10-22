6e9ba40ee807021f65e3851a52ef84e7b250f65c
shahidfoy <shahidfoy@gmail.com>
2023-10-22 14:53:42 -0600

    liquibase demo

---
+++ readme.md
@@ -0,0 +1,7 @@
+# Liquibase Demo
+
+Getting started with liquibase: 
+https://www.liquibase.org/get-started/best-practices \
+Create table: https://docs.liquibase.com/change-types/create-table.html \
+Add column: https://docs.liquibase.com/change-types/add-column.html \
+Insert: https://docs.liquibase.com/change-types/insert.html 
---
+++ src/main/java/com/shahidfoy/liquibasedemo/LiquibaseDemoApplication.java
@@ -0,0 +1,14 @@
+package com.shahidfoy.liquibasedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseDemoApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Starting...");
+		SpringApplication.run(LiquibaseDemoApplication.class, args);
+	}
+
+}
